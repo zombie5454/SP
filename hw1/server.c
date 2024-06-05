687ad2264878530f68f52fcbbb5f40d470a10025
zombie5454 <b08902129@ntu.edu.tw>
2024-06-05 17:41:11 +0800

    code review

--- hw1/server.c
+++ hw1/server.c
@@ -51,15 +51,15 @@ typedef struct {
 }registerRecord;
 
 int handle_read(request* reqP) {
-    int r;
+
     char buf[512];
 
     // Read in request from client
-    r = read(reqP->conn_fd, buf, sizeof(buf));
-    printf("in function handle read\n");
-    printf("%c%c%c\n", buf[0], buf[1], buf[2]);
-    if (r < 0) return -1;
-    if (r == 0) return 0;
+    if(read(reqP->conn_fd, buf, sizeof(buf)) < 0){
+        printf("Read from client fail\n");
+        return -1;
+    }
+
     char* p1 = strstr(buf, "\015\012");
     int newline_len = 2;
     if (p1 == NULL) {
@@ -73,6 +73,30 @@ int handle_read(request* reqP) {
     reqP->buf[len - 1] = '\0';
     reqP->buf_len = len-1;
     return 1;
+}
+
+void set_rdlock(struct flock *lock, off_t start){
+    lock->l_type = F_RDLCK;
+    lock->l_start = start;
+    lock->l_whence = SEEK_SET;
+    lock->l_len = sizeof(registerRecord);
+    lock->l_pid = getpid();
+}
+
+void set_wrlock(struct flock *lock, off_t start){
+    lock->l_type = F_WRLCK;
+    lock->l_start = start;
+    lock->l_whence = SEEK_SET;
+    lock->l_len = sizeof(registerRecord);
+    lock->l_pid = getpid();
+}
+
+void set_unlock(struct flock *lock, off_t start){
+    lock->l_type = F_UNLCK;
+    lock->l_start = start;
+    lock->l_whence = SEEK_SET;
+    lock->l_len = sizeof(registerRecord);
+    lock->l_pid = getpid();
 }
 
 int main(int argc, char** argv) {
@@ -91,13 +115,8 @@ int main(int argc, char** argv) {
     char buf[512];
     int buf_len;
 
-    int wrlock_in_process[20];
-    //int rdlock_in_process[20];
-    for(int i = 0; i < 20; i++){
-        wrlock_in_process[i] = 0;
-    //    rdlock_in_process[i] = 0;
-    }
-
+    int wrlock_in_process[20] = {0};      // in-process lock
+    struct flock lock;      // muti-process lock
 
     // Initialize server
     init_server((unsigned short) atoi(argv[1]));
@@ -109,38 +128,34 @@ int main(int argc, char** argv) {
 
     FD_ZERO(&original_set);
     FD_SET(svr.listen_fd, &original_set);
-    struct flock lock;
-
+   
     while (1) {
         // TODO: Add IO multiplexing
-    //    printf("maxfd = %d\n", maxfd);
-        // Check new connection
 
         memcpy(&working_set, &original_set, sizeof(original_set));
         if(select(maxfd, &working_set, NULL, NULL, NULL) <= 0)
             continue;
 
-        if(FD_ISSET(svr.listen_fd, &working_set)){
-        printf("deal with connect\n");
-        clilen = sizeof(cliaddr);
-        conn_fd = accept(svr.listen_fd, (struct sockaddr*)&cliaddr, (socklen_t*)&clilen);
-        if (conn_fd < 0) {
-            if (errno == EINTR || errno == EAGAIN) continue;  // try again
-            if (errno == ENFILE) {
-                (void) fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
-                continue;
+        if(FD_ISSET(svr.listen_fd, &working_set)){      // Check new connection
+            clilen = sizeof(cliaddr);
+            conn_fd = accept(svr.listen_fd, (struct sockaddr*)&cliaddr, (socklen_t*)&clilen);
+            if (conn_fd < 0) {
+                if (errno == EINTR || errno == EAGAIN) continue;  // try again
+                if (errno == ENFILE) {
+                    (void) fprintf(stderr, "out of file descriptor table ... (maxconn %d)\n", maxfd);
+                    continue;
+                }
+                ERR_EXIT("accept");
             }
-            ERR_EXIT("accept");
-        }
-        requestP[conn_fd].conn_fd = conn_fd;
-        strcpy(requestP[conn_fd].host, inet_ntoa(cliaddr.sin_addr));
-        fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd, requestP[conn_fd].host);
-        //我寫的
-        write(requestP[conn_fd].conn_fd, "Please enter your id (to check your preference order):\n", strlen("Please enter your id (to check your preference order):\n"));
-        FD_SET(conn_fd, &original_set);
-        printf("fd = %d\n", conn_fd);
-        //
-        continue;
+            requestP[conn_fd].conn_fd = conn_fd;
+            strcpy(requestP[conn_fd].host, inet_ntoa(cliaddr.sin_addr));
+            fprintf(stderr, "getting a new request... fd %d from %s\n", conn_fd, requestP[conn_fd].host);
+            //我寫的
+            write(requestP[conn_fd].conn_fd, "Please enter your id (to check your preference order):\n", strlen("Please enter your id (to check your preference order):\n"));
+            FD_SET(conn_fd, &original_set);
+            printf("fd = %d\n", conn_fd);
+            //
+            continue;
         }
 
         conn_fd = -1;
@@ -153,11 +168,7 @@ int main(int argc, char** argv) {
         if(conn_fd == -1)
             continue;
 
-        printf("deal with request\n");
-        printf("fd = %d\n", conn_fd);
-        int ret = handle_read(&requestP[conn_fd]); // parse data from client to requestP[conn_fd].buf
-        fprintf(stderr, "ret = %d\n", ret);
-	    if (ret < 0) {
+        if(handle_read(&requestP[conn_fd]) < 0){ // parse data from client to requestP[conn_fd].buf
             fprintf(stderr, "bad request from %s\n", requestP[conn_fd].host);
             continue;
         }
@@ -168,7 +179,7 @@ int main(int argc, char** argv) {
 
         
         requestP[conn_fd].id = atoi(requestP[conn_fd].buf);
-        if(902001 > requestP[conn_fd].id || 902020 < requestP[conn_fd].id){
+        if(902001 > requestP[conn_fd].id || 902020 < requestP[conn_fd].id){     // handle error input
             printf("[Error] Operation failed. Please try again.\n");
             write(conn_fd, "[Error] Operation failed. Please try again.\n", sizeof("[Error] Operation failed. Please try again.\n"));
             FD_CLR(conn_fd, &original_set);
@@ -176,160 +187,118 @@ int main(int argc, char** argv) {
             free_request(&requestP[conn_fd]);
             continue;
         }
-
-        lock.l_type = F_RDLCK;
-        lock.l_start = sizeof(registerRecord) * (requestP[conn_fd].id - 902001);
-        lock.l_whence = SEEK_SET;
-        lock.l_len = sizeof(registerRecord);
+        set_rdlock(&lock, sizeof(registerRecord) * (requestP[conn_fd].id - 902001));
         file_fd = open("registerRecord", O_RDONLY);
         if(fcntl(file_fd, F_SETLK, &lock) != -1 && wrlock_in_process[requestP[conn_fd].id - 902001] == 0){
-        
-        
-        registerRecord current_record;
-        lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
-        read(file_fd, &current_record, sizeof(registerRecord));
-        lock.l_type = F_UNLCK;
-        lock.l_start = sizeof(registerRecord) * (requestP[conn_fd].id - 902001);
-        lock.l_whence = SEEK_SET;
-        lock.l_len = sizeof(registerRecord);
-        fcntl(file_fd, F_SETLK, &lock);
-        printf("current id = %d, requestP id = %d\n", current_record.id, requestP[conn_fd].id);
-        char string[4][100];
-        strcpy(string[current_record.AZ], "AZ");
-        strcpy(string[current_record.BNT], "BNT");
-        strcpy(string[current_record.Moderna], "Moderna");
-        sprintf(string[0], "Your preference order is %s > %s > %s.\n", string[1], string[2], string[3]);
-        write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
-        close(file_fd);
-        }
-        else{
-            close(file_fd);
-            printf("Locked.\n");
-            write(conn_fd, "Locked.\n", sizeof("Locked.\n"));
-        }
-
-/*        fprintf(stderr, "%s", requestP[conn_fd].buf);
-        sprintf(buf,"%s : %s",accept_read_header,requestP[conn_fd].buf);
-        write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
-*/
-#elif defined WRITE_SERVER
-        if(requestP[conn_fd].buf[1] != ' '){
-        
-        requestP[conn_fd].id = atoi(requestP[conn_fd].buf);
-        if(902001 > requestP[conn_fd].id || 902020 < requestP[conn_fd].id){
-            printf("[Error] Operation failed. Please try again.\n");
-            write(conn_fd, "[Error] Operation failed. Please try again.\n", sizeof("[Error] Operation failed. Please try again.\n"));
-            FD_CLR(conn_fd, &original_set);
-            close(requestP[conn_fd].conn_fd);
-            free_request(&requestP[conn_fd]);
-            continue;
-        }
-        lock.l_type = F_WRLCK;
-        lock.l_start = sizeof(registerRecord) * (requestP[conn_fd].id - 902001);
-        lock.l_whence = SEEK_SET;
-        lock.l_len = sizeof(registerRecord);
-        lock.l_pid = getpid();
-        file_fd = open("registerRecord", O_RDWR);
-        if(fcntl(file_fd, F_SETLK, &lock) != -1 && wrlock_in_process[requestP[conn_fd].id - 902001] == 0){
-        wrlock_in_process[requestP[conn_fd].id - 902001] = 1;
-        registerRecord current_record;
-        lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
-        read(file_fd, &current_record, sizeof(registerRecord));
-
-        printf("current id = %d, requestP id = %d\n", current_record.id, requestP[conn_fd].id);
-        char string[4][100];
-        strcpy(string[current_record.AZ], "AZ");
-        strcpy(string[current_record.BNT], "BNT");
-        strcpy(string[current_record.Moderna], "Moderna");
-        sprintf(string[0], "Your preference order is %s > %s > %s.\n", string[1], string[2], string[3]);
-        write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
-        write(conn_fd, "Please input your preference order respectively(AZ,BNT,Moderna):\n", sizeof("Please input your preference order respectively(AZ,BNT,Moderna):\n"));
-        requestP[conn_fd].buf[1] = ' ';
-        continue;
-        }
-        
-        else{
-            close(file_fd);
-            printf("Locked.\n");
-            write(conn_fd, "Locked.\n", sizeof("Locked.\n"));
-        }
-
-
-        }
-
-        else{
-        
-        printf("process 2 in write\n");
-        file_fd = open("registerRecord", O_RDWR);
-
-        registerRecord current_record;
-        lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
-        read(file_fd, &current_record, sizeof(registerRecord));
-        printf("current id = %d, requestP id = %d\n", current_record.id, requestP[conn_fd].id);
-        char string[4][100];
-        strcpy(buf, requestP[conn_fd].buf);
-        printf("%s\n", buf);
-        int priority[3];
-        char numstr[10];
-        numstr[0] = buf[0];
-        numstr[1] = '\0';
-        priority[0] = atoi(numstr);
-        numstr[0] = buf[2];
-        numstr[1] = '\0';
-        priority[1] = atoi(numstr);
-        numstr[0] = buf[4];
-        numstr[1] = '\0';
-        priority[2] = atoi(numstr);
-        if(buf[1] != ' ' || buf[3] != ' ' ||
-        priority[0] > 3 || priority[1] > 3 || priority[2] > 3 ||
-        priority[0] < 1 || priority[1] < 1 || priority[2] < 1 ||
-        strlen(buf) > 5
-        ){
-            printf("[Error] Operation failed. Please try again.\n");
-            write(conn_fd, "[Error] Operation failed. Please try again.\n", sizeof("[Error] Operation failed. Please try again.\n"));
-            FD_CLR(conn_fd, &original_set);
-            close(requestP[conn_fd].conn_fd);
-            free_request(&requestP[conn_fd]);
-            continue;
-        }
-        lseek(file_fd, -sizeof(registerRecord), SEEK_CUR);
-        current_record.AZ = priority[0];
-        current_record.BNT = priority[1];
-        current_record.Moderna = priority[2];
-        if(write(file_fd, &current_record, sizeof(registerRecord)) == -1)
-            printf("write to record fail\n");
-        /*else
-            printf("write to record success\n");
-        printf("%d %d %d\n", priority[0], priority[1], priority[2]);
-        
-        lseek(file_fd, -sizeof(registerRecord), SEEK_CUR);
-        for(int i = 0; i < 20; i++){
+            // if get rdlock
+            registerRecord current_record;
+            lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
             read(file_fd, &current_record, sizeof(registerRecord));
-            if(current_record.id == requestP[conn_fd].id){
-                break;
-            }
-        }*/
-        strcpy(string[current_record.AZ], "AZ");
-        strcpy(string[current_record.BNT], "BNT");
-        strcpy(string[current_record.Moderna], "Moderna");
-        sprintf(string[0], "Preference order for %d modified successed, new preference order is %s > %s > %s.\n", requestP[conn_fd].id, string[1], string[2], string[3]);
-        printf("%s\n", string[0]);
-        write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
-        lock.l_type = F_UNLCK;
-        lock.l_start = sizeof(registerRecord) * (requestP[conn_fd].id - 902001);
-        lock.l_whence = SEEK_SET;
-        lock.l_len = sizeof(registerRecord);
-        lock.l_pid = getpid();
-        fcntl(file_fd, F_SETLK, &lock);
-        wrlock_in_process[requestP[conn_fd].id - 902001] = 0;
-        close(file_fd);
+            set_unlock(&lock, sizeof(registerRecord) * (requestP[conn_fd].id - 902001));
+            fcntl(file_fd, F_SETLK, &lock);
+            char string[4][100];
+            strcpy(string[current_record.AZ], "AZ");
+            strcpy(string[current_record.BNT], "BNT");
+            strcpy(string[current_record.Moderna], "Moderna");
+            sprintf(string[0], "Your preference order is %s > %s > %s.\n", string[1], string[2], string[3]);
+            write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
+            close(file_fd);
+        }
+        else{   // if already wrlocked
+            close(file_fd);
+            printf("try to read %d but Locked.\n", requestP[conn_fd].id);
+            write(conn_fd, "Locked.\n", sizeof("Locked.\n"));
+        }
 
+#elif defined WRITE_SERVER
+        if(requestP[conn_fd].buf[1] != ' '){    // process 1 in write: read id
+            requestP[conn_fd].id = atoi(requestP[conn_fd].buf);
+            if(902001 > requestP[conn_fd].id || 902020 < requestP[conn_fd].id){     // handle error input
+                printf("[Error] Operation failed. Please try again.\n");
+                write(conn_fd, "[Error] Operation failed. Please try again.\n", sizeof("[Error] Operation failed. Please try again.\n"));
+                FD_CLR(conn_fd, &original_set);
+                close(requestP[conn_fd].conn_fd);
+                free_request(&requestP[conn_fd]);
+                continue;
+            }
+            set_wrlock(&lock, sizeof(registerRecord) * (requestP[conn_fd].id - 902001));
+            file_fd = open("registerRecord", O_RDWR);
+            if(fcntl(file_fd, F_SETLK, &lock) != -1 && wrlock_in_process[requestP[conn_fd].id - 902001] == 0){  
+                // if no lock, then get wrlock
+                wrlock_in_process[requestP[conn_fd].id - 902001] = 1;
+                registerRecord current_record;
+                lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
+                read(file_fd, &current_record, sizeof(registerRecord));
+
+                char string[4][100];
+                strcpy(string[current_record.AZ], "AZ");
+                strcpy(string[current_record.BNT], "BNT");
+                strcpy(string[current_record.Moderna], "Moderna");
+                sprintf(string[0], "Your preference order is %s > %s > %s.\n", string[1], string[2], string[3]);
+                write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
+                write(conn_fd, "Please input your preference order respectively(AZ,BNT,Moderna):\n", sizeof("Please input your preference order respectively(AZ,BNT,Moderna):\n"));
+                requestP[conn_fd].buf[1] = ' ';
+                continue;
+            }
+            
+            else{   // if already locked
+                close(file_fd);
+                printf("try to write %d but Locked.\n", requestP[conn_fd].id);
+                write(conn_fd, "Locked.\n", sizeof("Locked.\n"));
+            }
+        }
+
+        else{           // process 2 in write: read priority 
+        
+            file_fd = open("registerRecord", O_RDWR);
+            registerRecord current_record;
+            lseek(file_fd, sizeof(registerRecord)*(requestP[conn_fd].id-902001), SEEK_SET);
+            read(file_fd, &current_record, sizeof(registerRecord));
+            char string[4][100];
+            strcpy(buf, requestP[conn_fd].buf);
+            printf("%s\n", buf);
+            int priority[3];
+            char numstr[10];
+            numstr[0] = buf[0];
+            numstr[1] = '\0';
+            priority[0] = atoi(numstr);
+            numstr[0] = buf[2];
+            numstr[1] = '\0';
+            priority[1] = atoi(numstr);
+            numstr[0] = buf[4];
+            numstr[1] = '\0';
+            priority[2] = atoi(numstr);
+            if(buf[1] != ' ' || buf[3] != ' ' ||
+            priority[0] > 3 || priority[1] > 3 || priority[2] > 3 ||
+            priority[0] < 1 || priority[1] < 1 || priority[2] < 1 ||
+            strlen(buf) > 5){
+                printf("[Error] Operation failed. Please try again.\n");
+                write(conn_fd, "[Error] Operation failed. Please try again.\n", sizeof("[Error] Operation failed. Please try again.\n"));
+                FD_CLR(conn_fd, &original_set);
+                close(requestP[conn_fd].conn_fd);
+                free_request(&requestP[conn_fd]);
+                continue;
+            }
+            lseek(file_fd, -sizeof(registerRecord), SEEK_CUR);
+            current_record.AZ = priority[0];
+            current_record.BNT = priority[1];
+            current_record.Moderna = priority[2];
+            if(write(file_fd, &current_record, sizeof(registerRecord)) == -1)
+                printf("write to record fail\n");
+    
+            strcpy(string[current_record.AZ], "AZ");
+            strcpy(string[current_record.BNT], "BNT");
+            strcpy(string[current_record.Moderna], "Moderna");
+            sprintf(string[0], "Preference order for %d modified successed, new preference order is %s > %s > %s.\n", requestP[conn_fd].id, string[1], string[2], string[3]);
+            printf("%s\n", string[0]);
+            write(conn_fd, string[0], sizeof(char)*strlen(string[0]));
+            set_unlock(&lock, sizeof(registerRecord) * (requestP[conn_fd].id - 902001));
+            fcntl(file_fd, F_SETLK, &lock);
+            wrlock_in_process[requestP[conn_fd].id - 902001] = 0;
+            close(file_fd);
 
         }
-/*        fprintf(stderr, "%s", requestP[conn_fd].buf);
-        sprintf(buf,"%s : %s",accept_write_header,requestP[conn_fd].buf);
-        write(requestP[conn_fd].conn_fd, buf, strlen(buf));    
-*/     
+
 #endif
         FD_CLR(conn_fd, &original_set);
         close(requestP[conn_fd].conn_fd);
