42efcc1d691640d44617c3a11ec1e4131187b75f
zombie5454 <b08902129@ntu.edu.tw>
2024-06-15 14:25:27 +0800

    set GuessRound to 1

--- hw2/player.c
+++ hw2/player.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<string.h>
+#define GUESSROUND 1
 
 int main(int argc, char *argv[]){
+    if(strncmp(argv[1], "-n", 2) != 0){
+        fputs("command error\n", stderr);
+        return -1;
+    }
     int player_id = atoi(argv[2]);
     int guess;
+    int guess_round = GUESSROUND;
 /* initialize random seed: */
-    for(int i = 1; i <= 10; i++){
+    for(int i = 1; i <= guess_round; i++){
 /* generate guess between 1 and 1000: */
         srand ((player_id + i) * 323);
         char message[20];
